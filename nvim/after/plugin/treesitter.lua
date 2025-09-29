f4e36cf6650f719653595f76df9057a94d7d7a53
davipuddo <davifpuddo@gmail.com>
2025-09-29 20:09:46 -0300

    fix merge error

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,10 +1,6 @@
 require('nvim-treesitter.configs').setup {
   -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-<<<<<<< HEAD
   ensure_installed = { "c", "vim", "lua" },
-=======
-  ensure_installed = { "c", "vim", "lua", "vimdoc", "markdown", "markdown_inline" },
->>>>>>> 1cac5a31e9f3b598550768bab77cddec797cd581
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
